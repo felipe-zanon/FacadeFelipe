a01ba0cb4936916f4acb45ee7d21cf9798655fb3
Felipe Zanon <126215505+felipe-zanon@users.noreply.github.com>
2024-09-22 15:53:56 -0300

    Commit Projeto

---
+++ TesteFacade/Produto.java
@@ -0,0 +1,6 @@
+package Aula6.com;
+
+public class Produto {
+    private String descricao;
+    private double preco;
+}
---
+++ TesteFacade/Sobremesa.java
@@ -0,0 +1,21 @@
+package Aula6.com;
+
+import java.util.Scanner;
+
+public class Sobremesa implements Produto{
+
+    private String tamanho;
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+
+
+}
+    
+
